210eec569969fccd2f6d111a5a8851e78d25b6f5
vinipis <cvf.999@gmail.com>
2018-09-03 17:43:49 -0300

    Criação do diretorio para curso em Java

---
+++ Cursojava/src/fundamentos/comentario.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+/**
+ * Classe responsavel por mostrar o uso dos 3 tipos de comentarios 
+ * @author vinicius
+ * @version 1.0
+ * @since 2.2
+ */
+public class comentario {
+	
+	/**
+	 * Esse metodo é a porta de entrada de um programa Java.
+	 * @param args parametros passados por  linha de comando 
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Imprime no console a palavra primeiro e
+		 * enseguida cria uma nova linha
+		 * */
+		System.out.println("Primeiro");
+		
+		//Cuidado com os comentarios óbvios
+		System.out.println("Programa!");
+	}
+}
