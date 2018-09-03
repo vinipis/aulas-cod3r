58ad2467042868196681fbf102422cd555d5a1c9
vinipis <cvf.999@gmail.com>
2018-09-03 18:59:25 -0300

    Aprendendo sobre imports

---
+++ Cursojava/src/fundamentos/Import.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+import java.awt.Button;
+import java.sql.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		Double a = 2.0;
+		System.out.println(a);
+		
+		Button b;
+		Date d;
+	}
+}
+
+//ctrl + shift + o para ajudar com os imports, tanto o que precisa como o que não precisa
