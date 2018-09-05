172a298ea7c9085dfdf2181b9ff50a02fe94051a
vinipis <cvf.999@gmail.com>
2018-09-04 22:24:01 -0300

    Operador ternario

---
+++ Cursojava/src/fundamentos/OperadorTernario.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class OperadorTernario {
+
+	public static void main(String[] args) {
+		double nota = 6.1;
+		String resultado = nota >= 7 ? "Aprovado" : "Recuperação";
+		System.out.println(resultado);
+	}
+}
