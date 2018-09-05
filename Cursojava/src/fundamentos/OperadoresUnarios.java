b8a6ebe959e28fc1f55b3cc99c4c8ad577bf81e0
vinipis <cvf.999@gmail.com>
2018-09-04 22:18:14 -0300

    Operadores Unarios

---
+++ Cursojava/src/fundamentos/OperadoresUnarios.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		int num2 = 2;
+		
+		//Incremento e decremento
+		num1++;
+		System.out.println(num1);
+		--num1;
+		System.out.println(num1);
+		
+		System.out.println(++num1 == num2--);
+		System.out.println(num1 == num2);
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		//Complemento binario
+		System.out.println(Integer.toBinaryString(50));
+		System.out.println(Integer.toBinaryString(~50));
+		
+		//Negaçao
+		System.out.println(!true);
+	}
+}
