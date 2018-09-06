390829cfb111187a8cf0d213dee66e9cc633e975
vinipis <cvf.999@gmail.com>
2018-09-06 10:33:53 -0300

    Iniciando controles de estruturas

---
+++ Cursojava/src/controle/If.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class If {
+
+	public static void main(String[] args) {
+		double nota = 9.5;
+		boolean bomConportamento = false;
+		if (nota >= 9 && bomConportamento)
+			System.out.println("Quadro de honra!!");
+	}
+}
---
+++ Cursojava/src/controle/IfElse.java
@@ -0,0 +1,13 @@
+package controle;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		double nota = 6.5;
+
+		if (nota >= 7)
+			System.out.println("Aprovado");
+		else
+			System.out.println("Reprovado");
+	}
+}
