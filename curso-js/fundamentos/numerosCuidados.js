01c0ab18b409806b8454b8a62be2fb9b0b6b8005
vinipis <cvf.999@gmail.com>
2019-01-15 21:37:34 -0200

    continuação dos fundamentos

---
+++ curso-js/fundamentos/numerosCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //Infinity
+console.log("10" / 2)
+console.log("3" + 2)
+console.log("show!" * 2)
+console.log(0.1 + 0.7)
+//console.log(10.ToString())
+console.log((10.345).toFixed(2))
