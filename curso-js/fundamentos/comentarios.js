19e57f51930e29c03c2b6a0d4ae1c6f5fed60616
vinipis <cvf.999@gmail.com>
2019-01-15 20:10:44 -0200

    Começando com JS

---
+++ curso-js/fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+// comentario de uma linha
+console.log('linha 1')
+
+/*
+Comentario de 
+muiltiplas linhas
+*/
+console.log('linha 2')
+
+/*
+*comentario de 
+*multiplas linhas
+*/
+console.log('linha 3')
---
+++ curso-js/fundamentos/organizaçao.js
@@ -0,0 +1,8 @@
+console.log('Sentença de codigo')
+
+{
+    {
+        console.log("Ola");
+        console.log('Mundo!') // Padrão do cusro
+    }
+}
